879f44c8f8a10ac4d9c76b95f474727de44f5c85
Tim Forkmann <tim.forkmann@kraftwerk.io>
2025-10-15 20:09:15 -0400

    refactor: replace @shopify/react-native-skia with react-konva and restructure project files

--- src/Feliz.Konva/Interop.fs
+++ src/Feliz.Konva/Interop.fs
@@ -12,9 +12,9 @@ module Interop =
     let inline mkStylesProp (key: string) (value: obj) : IKonvaStylesProp = unbox (key, value)
     let inline mkOptionsProp (key: string) (value: obj) : IOptionsProp = unbox (key, value)
 
-    let skia: obj = importAll "@shopify/react-native-skia/lib/module/web"
+    let skia: obj = importAll "react-konva/lib/module/web"
 
-    let canvas: obj = import "Canvas" "@shopify/react-native-skia/lib/module"
-    let circle: obj = import "Circle" "@shopify/react-native-skia/lib/module"
-    let rect:   obj = import "Rect"   "@shopify/react-native-skia/lib/module"
+    let canvas: obj = import "Canvas" "react-konva/lib/module"
+    let circle: obj = import "Circle" "react-konva/lib/module"
+    let rect:   obj = import "Rect"   "react-konva/lib/module"
     skia?LoadKonvaWeb()
--- vite.config.mts
+++ vite.config.mts
@@ -32,7 +32,7 @@ export default defineConfig(({ command, mode, isSsrBuild, isPreview  }) => {
         },
         resolve: { alias: {} },
         optimizeDeps: {
-        include: ["@shopify/react-native-skia"]
+        include: ["react-konva"]
         },
         assetsInclude: ["**/*.wasm"],
         server: {
